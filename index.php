545ea89f79624466165376f04663df57c2b7a002
Lucxol69 <your.email@example.com>
2025-11-04 14:14:51 +0100

    formulaire

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="fr" prefix="og: https://ogp.me/ns#">
+<head>
+  <meta charset="utf-8" />
+  <meta name="author" content="Lucas Magat-Dimek" />
+  <meta name="keywords" content="document html5 mmi" />
+  <meta name="description" content="Document minimal html5 en DUT MMI" />
+  <!-- Affichage standard sur périphérique mobile pour site adaptatif -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Données Open Graph -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="http://mmi.univ-smb.fr/~mmang/" />
+  <meta property="og:image" content="http://mmi.univ-smb.fr/~mmang/image.png" />
+  <meta property="og:locale" content="fr_FR" />
+  <meta property="og:description" content="Document minimal html5 en DUT MMI" />
+  <meta property="og:title" content="Document minimal en MMI" />
+  <meta property="og:site_name" content="Document minimal en MMI" />
+  <title>Document minimal en MMI</title>
+</head>
+
+
+<body>
+    <header>
+        <!-- en-tête -->
+            <h1>Document minimal en MMI</h1>
+        <nav>
+        <!-- menu principal -->
+        </nav>
+     </header>        
+        
+        <!-- partie principale -->
+        <section>
+            <h2>Titre de section</h2>
+         </section>
+<section>
+            <form action="index.php" method="post">
+                <label for="identifiant">Identifiant :</label>
+                <input type="text" id="identifiant" name="identifiant" required>
+                <br>
+                <label for="motdepasse">Mot de passe :</label>
+                <input type="password" id="motdepasse" name="motdepasse" required>
+                <br>
+                <button type="submit">Envoyer</button>
+            </form>
+
+</section>
+
+
+        
+        <footer>
+        <!-- pied de page -->
+            <p>Copyright © </p>
+        </footer>
+</body>
+</html>
